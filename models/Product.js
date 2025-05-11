819daa61122c312b4b02ddd97edfd84f24a219a3
Omary Haruna <ommyhrn15gmi@gmail.com@example.com>
2025-05-11 12:27:07 +0300

    backend

--- models/Product.js
+++ models/Product.js
@@ -1,13 +1,30 @@
 const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
-    name: { type: String, required: true },
+    name: {
+        type: String,
+        required: true,
+    },
     category: String,
-    buyingPrice: { type: Number },
-    sellingPrice: { type: Number },
-    quantity: { type: Number },
+    buyingPrice: {
+        type: Number,
+        required: true, // ✅ VERY IMPORTANT for calculating profit
+    },
+    sellingPrice: {
+        type: Number,
+        required: true,
+    },
+    quantity: {
+        type: Number,
+        required: true,
+    },
     image: String,
-    images: [String]
+    images: [String],
+    dateAdded: {
+        type: Date,
+        default: Date.now,
+    },
+    dateModified: Date
 });
 
 module.exports = mongoose.model('Product', productSchema);
